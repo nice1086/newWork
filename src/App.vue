5dbdc4b5d096fb53fa9b3be787523318f26eddc2
nice <nicenight.com>
2023-10-30 16:32:26 +0800

    ""

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <FirstComponent msg="hello"/>
+  <HelloWorld msg="hey"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FirstComponent from './components/FirstComponent.vue';
+import HelloWorld from './components/HelloWorld.vue';
+
 
 export default {
   name: 'App',
   components: {
+    FirstComponent,
     HelloWorld
-  }
+}
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
